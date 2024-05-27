6f4e0e10a602cf05045ae1f05b90e8a663a8f9c0
VedantBhagat28 <vedantbhagat2004@gmail.com>
2024-05-27 11:05:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ToDoList from "./Todolist.jsx"
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
